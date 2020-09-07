dba36c5b0b28bc9dd166ce2a23d4fccf1bca29fb
Pitkuha <Danik2000@bk.ru>
2020-09-07 17:19:54 +0300

    Завёз комментарии

---
+++ tree.pl
@@ -0,0 +1,38 @@
+% Мужчины в семье
+man("Dan_Pitkovkiy").
+man("Dmitriy_Pitkovkiy").
+man("Leonid_Plotkin").
+man("Itay_Plotkin").
+man("Yakov_Plotkin").
+man("Lev_Pitkovkiy").
+man("Naum_Plotkin").
+man("Moisey_Pitkovkiy").
+man("Mihail_Pitkovkiy").
+man("Arkadiy_Zharkov").
+man("Eugene_Zharkov").
+% Женщины в семье
+woman("Galina_Pitkovskaya").
+woman("Eugenya_Plotkina").
+woman("Irina_Plotkina").
+woman("Margarita_Pitkovskaya").
+woman("Shelly_Plotkin").
+woman("Galina_Plotkina").
+woman("Anna_Zharkova").
+woman("Lyubov_Zharkova").
+woman("Miri_Zharkova").
+woman("Irena_Zharkova").
+% Супруги
+spouses("Dmitriy_Pitkovkiy","Galina_Pitkovskaya").
+spouses("Leonid_Plotkin","Irina_Plotkina").
+spouses("Yakov_Plotkin","Eugenya_Plotkina").
+spouses("Lev_Pitkovkiy","Margarita_Pitkovskaya").
+spouses("Naum_Plotkin","Galina_Plotkina").
+spouses("Arkadiy_Zharkov","Anna_Zharkova").
+spouses("Eugene_Zharkov","Lyubov_Zharkova").
+
+
+% Родители
+
+
+
+
